b5f6f46d9c21b962284efb238ad9b2a5b72a34e0
IT21177200 <99163691+IT21177200@users.noreply.github.com>
2022-05-19 23:31:44 +0530

    Add files via upload

---
+++ admin.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+
+using namespace std;
+
+class Admin{
+	private:
+		int admin_ID;
+		string Name;
+	
+	public:
+		Admin();
+		Admin(int aID,string aName);
+		void displayAdminDetails();
+}; 
+
+Admin::Admin()
+{
+	admin_ID=0;
+	Name="";
+}
+
+Admin::Admin(int aID,string aName)
+{
+	admin_ID=aID;
+	Name=aName;
+}
+
+void Admin::displayAdminDetails()
+{
+	cout<<"Admin Details : "<<endl;
+	cout<<"   Admin ID : "<<admin_ID<<endl;
+	cout<<"   Admin Name : "<<Name<<endl;
+}
+
+int main()
+{
+	//Admin 
+	Admin ad1(997788,"Thiwanka Suresh");
+	Admin ad2(887766,"Suresh Maliyadde");
+	
+	ad1.displayAdminDetails();
+	cout<<endl;
+	
+	return 0;
+}
